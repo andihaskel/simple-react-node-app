38c318857c569125c8b0240bc39150f1310a2083
Andres Haskel <andres.haskel@gmail.com>
2020-09-16 23:53:11 -0300

    ngrox & axios integration

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -4,12 +4,24 @@ const { port, env } = require('./config/vars');
 const logger = require('./config/logger');
 const app = require('./config/express');
 const mongoose = require('./config/mongoose');
+const ngrok = require('ngrok');
 
 // open mongoose connection
 mongoose.connect();
 
 // listen to requests
-app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+app.listen(port, () => {
+    logger.info(`server started on port ${port} (${env})`);
+    try {
+        (async function () {
+            const ngrokEndpoint = await ngrok.connect(port);
+            logger.info(`localhost: ${port} is available at ${ngrokEndpoint}`);
+        })()
+    } catch (ex){
+        logger.error(err);
+    }
+
+});
 
 /**
 * Exports express
--- front-end/src/components/eventForm/submit.js
+++ front-end/src/components/eventForm/submit.js
@@ -1,19 +1,26 @@
 import { SubmissionError } from 'redux-form'
+const axios = require('axios');
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-function submit(values) {
-    console.log(values);
-  return sleep(1000) // simulate server latency
-    .then(() => {
-    //   if (![ 'john', 'paul', 'george', 'ringo' ].includes(values.username)) {
-    //     throw new SubmissionError({ username: 'User does not exist', _error: 'Login failed!' })
-    //   } else if (values.password !== 'redux-form') {
-    //     throw new SubmissionError({ password: 'Wrong password', _error: 'Login failed!' })
-    //   } else {
-        window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
-      
-    })
+async function submit(values) {
+    console.log('submit',values);
+    return sleep(1000) // simulate server latency
+        .then(async () => {
+            let res = await axios.post('https://2edf1a0ff52f.ngrok.io/v1/events', values);
+
+            console.log('res', res)
+            console.log(`Status code: ${res.status}`);
+            console.log(`Status text: ${res.statusText}`);
+            console.log(`Request method: ${res.request.method}`);
+            console.log(`Path: ${res.request.path}`);
+
+            console.log(`Date: ${res.headers.date}`);
+            console.log(`Data: ${res.data}`);
+
+            window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+
+        })
 }
 
 export default submit
